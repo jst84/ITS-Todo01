1e6538fa3f6ab99a760a10d96766492a08595762
Stefano Danzo <stefano.danzo@teorema.net>
2019-04-16 11:27:11 +0200

    added ui

---
+++ Todo/Todo.UI/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace Todo.UI.Controllers
+{
+    [RoutePrefix("api/todo")]
+    public class TodoController : ApiController
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Todo.DAL.Todo> GetAll()
+        {
+            DAL.DAL d = new DAL.DAL(System.Configuration.ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+
+            var todos = d.GetTodos();
+            return todos;
+        }
+    }
+}
